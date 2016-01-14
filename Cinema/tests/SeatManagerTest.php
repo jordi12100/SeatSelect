a53727581d453c73d3797bc9fdf849579592a9a2
Jordi Kroon <jordi@jordikroon.nl>
2016-01-14 20:28:29 +0100

    Added more tests

--- Cinema/tests/SeatManagerTest.php
+++ Cinema/tests/SeatManagerTest.php
@@ -18,9 +18,30 @@ class SeatManagerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals([1, 2, 3], $seatManager->getSeatNumbers(3));
     }
 
-    public function testAvailable()
+    /**
+     * @dataProvider seatsProvider
+     */
+    public function testAvailable($visitors, $seats, $chosenSeats, $expectation)
     {
-        $seatManager = new SeatManager(range(1, 10));
-        $this->assertEquals([1, 2, 3], $seatManager->getSeatNumbers(3));
+        $seatManager = new SeatManager($seats, $chosenSeats);
+        $this->assertEquals($expectation, $seatManager->getSeatNumbers($visitors));
+    }
+
+    /**
+     * visitors | seats | chosen seats | expectation
+     * @return array
+     */
+    public function seatsProvider()
+    {
+        return [
+            3, range(1, 10), [4, 5, 6], [1, 2, 3],
+            3, range(1, 10), [1, 2, 3, 7, 8, 9], [4, 5, 6],
+            3, range(1, 10), [1, 2, 3, 4, 5, 6, 7, 8], [9, 10, null],
+            1, range(1, 10), [1, 2, 3, 4, 6, 7, 8, 9, 10], [5],
+            3, range(1, 10), [1, 2, 4, 5, 6, 9, 10], [7, 8, 3],
+            2, range(1, 10), [1, 2, 4, 5, 6, 9, 10], [7, 8],
+            4, range(1, 10), [1, 2, 4, 5, 6, 9, 10], [7, 8, 3, null],
+            2, range(1, 10), [1, 2, 4, 5, 6, 7, 9, 10], [3, 8],
+        ];
     }
 }
