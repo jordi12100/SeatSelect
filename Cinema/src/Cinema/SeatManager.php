78a33b9cd87ee7d137574df9cd1c5ab4005ac9ff
Jordi Kroon <jordi@jordikroon.nl>
2016-01-14 23:37:36 +0100

    Added logic

--- Cinema/src/Cinema/SeatManager.php
+++ Cinema/src/Cinema/SeatManager.php
@@ -10,6 +10,11 @@ class SeatManager
     protected $seats = [];
 
     /**
+     * @var array
+     */
+    protected $chosenSeats = [];
+
+    /**
      * @param array $seats
      * @param array $chosenSeats
      */
@@ -25,9 +30,72 @@ class SeatManager
      */
     public function getSeatNumbers($visitors)
     {
-        if (count($this->getAvailableSeats())) {
-            return null;
+        $seatNumbers = $this->getBestSeatNumbers($visitors);
+        return count($seatNumbers) === $visitors ? $seatNumbers : null;
+    }
+
+    /**
+     * @param int $visitors
+     * @param null|int $seats
+     * @return array
+     */
+    protected function getBestSeatNumbers($visitors, $seats = null)
+    {
+        $seats = $seats ?: $visitors;
+
+        $bestSeats = $this->findBestSeatsFor($this->getAvailableSeats(), $seats);
+        if (count($bestSeats) !== $visitors && count($this->getAvailableSeats()) > 0) {
+            $bestSeats = array_merge($bestSeats, $this->getBestSeatNumbers($seats, $visitors-1 ?: 1));
         }
+
+        return $bestSeats;
+    }
+
+    /**
+     * @param array $availableSeats
+     * @param int $visitors
+     * @return null
+     */
+    protected function findBestSeatsFor($availableSeats, $visitors)
+    {
+        foreach ($availableSeats as $key => $availableSeat) {
+            $start = $key + 1;
+            $end = $start + ($visitors-1);
+
+            if (!$this->isRangeAvailable($start, $end)) {
+                continue;
+            }
+
+            $this->reserveSeats(range($start, $end));
+            return range($start, $end);
+        }
+
+        return [];
+    }
+
+    /**
+     * @param int $start
+     * @param int $end
+     * @return bool
+     */
+    protected function isRangeAvailable($start, $end)
+    {
+        for ($i = $start; $i <= $end; $i++) {
+            if (!$this->isAvailable($i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @param int $seat
+     * @return bool
+     */
+    protected function isAvailable($seat)
+    {
+        return in_array($seat, $this->getAvailableSeats());
     }
 
     /**
@@ -36,5 +104,18 @@ class SeatManager
     protected function getAvailableSeats()
     {
         return array_diff($this->seats, $this->chosenSeats);
+    }
+
+    /**
+     * @param array $seats
+     * @return array
+     */
+    protected function reserveSeats($seats)
+    {
+        foreach ($seats as $seat) {
+            $this->chosenSeats[] = $seat;
+        }
+
+        return $seats;
     }
 }
--- Cinema/tests/SeatManagerTest.php
+++ Cinema/tests/SeatManagerTest.php
@@ -14,7 +14,7 @@ class SeatManagerTest extends \PHPUnit_Framework_TestCase
 
     public function testAllSeatsAvailable()
     {
-        $seatManager = new SeatManager(range(1, 10));
+        $seatManager = new SeatManager(range(1, 10), []);
         $this->assertEquals([1, 2, 3], $seatManager->getSeatNumbers(3));
     }
 
@@ -34,14 +34,17 @@ class SeatManagerTest extends \PHPUnit_Framework_TestCase
     public function seatsProvider()
     {
         return [
-            3, range(1, 10), [4, 5, 6], [1, 2, 3],
-            3, range(1, 10), [1, 2, 3, 7, 8, 9], [4, 5, 6],
-            3, range(1, 10), [1, 2, 3, 4, 5, 6, 7, 8], [9, 10, null],
-            1, range(1, 10), [1, 2, 3, 4, 6, 7, 8, 9, 10], [5],
-            3, range(1, 10), [1, 2, 4, 5, 6, 9, 10], [7, 8, 3],
-            2, range(1, 10), [1, 2, 4, 5, 6, 9, 10], [7, 8],
-            4, range(1, 10), [1, 2, 4, 5, 6, 9, 10], [7, 8, 3, null],
-            2, range(1, 10), [1, 2, 4, 5, 6, 7, 9, 10], [3, 8],
+            [3, range(1, 10), [4, 5, 6], [1, 2, 3]],
+            [3, range(1, 10), [1, 2, 3, 7, 8, 9], [4, 5, 6]],
+            [1, range(1, 10), [1, 2, 3, 4, 6, 7, 8, 9, 10], [5]],
+
+            [3, range(1, 10), [1, 2, 4, 5, 6, 9, 10], [7, 8, 3]],
+
+            [2, range(1, 10), [1, 2, 4, 5, 6, 9, 10], [7, 8]],
+            [2, range(1, 10), [1, 2, 4, 5, 6, 7, 9, 10], [3, 8]],
+
+            [4, range(1, 10), [1, 2, 4, 5, 6, 9, 10], null],
+            [1, range(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], null],
         ];
     }
 }
